40205a2d18b3d7ea06d0a083f6b9bc87d2343ca4
Willem van Heemstra <wvanheemstra@icloud.com>
2024-07-25 13:29:55 +0000

    Increase width of diagram to 1920

--- public/js/data.js
+++ public/js/data.js
@@ -1,5 +1,6 @@
 var margin = [20, 120, 20, 140],
-    width = 1280 - margin[1] - margin[3],
+    // width = 1280 - margin[1] - margin[3],
+    width = 1920 - margin[1] - margin[3],
     height = 800 - margin[0] - margin[2],
     i = 0,
     duration = 1250,
